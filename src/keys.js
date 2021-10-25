b3ee504ae6b01471ce55b041450f959734d68a42
nodierUcaldas <nodier.1701115195@ucaldas.edu.co>
2021-10-25 18:45:45 -0500

    upload project
    se adicionan clases restantes

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: { //objeto de configuracion de la base de datos
+        connectionLimit: 10,
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'db_links'
+    }
+
+};
